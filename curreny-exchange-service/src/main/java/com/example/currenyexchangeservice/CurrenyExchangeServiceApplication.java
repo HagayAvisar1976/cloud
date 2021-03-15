6eb2cf873adfb55c94ca2ce8441a0bf27beb8596
havisar <havisar@ebay.com>
2021-03-15 14:55:01 +0200

    adding currency-exchange

---
+++ curreny-exchange-service/src/main/java/com/example/currenyexchangeservice/CurrenyExchangeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.currenyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrenyExchangeServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CurrenyExchangeServiceApplication.class, args);
+  }
+}
