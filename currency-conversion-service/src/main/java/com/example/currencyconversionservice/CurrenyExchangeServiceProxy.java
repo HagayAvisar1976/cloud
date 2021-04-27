91859b8948acff51c26037834d884cbe2d2762ad
havisar <havisar@ebay.com>
2021-04-27 12:30:28 +0300

    fix

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrenyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrenyExchangeServiceProxy.java
@@ -1,10 +1,13 @@
 package com.example.currencyconversionservice;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "currency-exchange-service", url ="localhost:8000" )
+//@FeignClient(name = "currency-exchange-service") // move it to configuration in order to talk with all instanaces
+//@RibbonClient(name = "currency-exchange-service")
 public interface CurrenyExchangeServiceProxy {
 
   @GetMapping("/currency-exchange/{from}/to/{to}")
