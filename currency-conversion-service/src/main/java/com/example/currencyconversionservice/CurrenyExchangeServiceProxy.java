0501b56d7ac4244c23df44154d98583cf3d1fe79
havisar <havisar@ebay.com>
2021-05-03 13:54:50 +0300

    connect exchange service to naming server and let Feign find the exchange server using Eureka.

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrenyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrenyExchangeServiceProxy.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url ="localhost:8000" )
-//@FeignClient(name = "currency-exchange-service") // move it to configuration in order to talk with all instanaces
+//@FeignClient(name = "currency-exchange-service", url ="localhost:8000" )
+@FeignClient(name = "currency-exchange-service") // now it is using the naming server (Eureka) in order to find the service
 //@RibbonClient(name = "currency-exchange-service")
 public interface CurrenyExchangeServiceProxy {
 
